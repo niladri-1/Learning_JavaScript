e9e29e405474985ee578c63bfd3366d7b95a871a
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-06 10:13:23 +0530

    Accept user input

---
+++ 04 Accept user input/script.js
@@ -0,0 +1,16 @@
+// How to accept user input
+
+// 1. EASY WAY = window promt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+// // 1. EASY WAY = window promt
+// let userName = window.prompt("What's Your username?");
+// console.log(`Your name is ${userName}`);
+
+// // 2. PROFESSIONAL WAY = HTML textbox
+let userName;
+
+document.getElementById("mySubmit").onclick = function(){
+    userName = document.getElementById("mytext").value;
+    document.getElementById("myh1").textContent = `Wellcome, ${userName}`
+}
