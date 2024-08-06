b9a1000e28f21bdc50eebbf4de5529e8722d5735
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-06 10:39:44 +0530

    Type conversion

---
+++ 05 Type conversion/script.js
@@ -0,0 +1,31 @@
+// Type conversion = chenge the datatype of a value to another
+//                   (string, numbers, booleans)
+
+// let age = window.prompt("How old are you ?");
+// age = Number(age);
+// age+=1;
+// console.log(age);
+
+let x;
+let y;
+let z;
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
