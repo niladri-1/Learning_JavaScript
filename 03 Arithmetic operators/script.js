1358e700a7d77db8203c5a70031b6f5e2d897ea6
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-06 08:41:07 +0530

    Arithmetic operators in JS

---
+++ 03 Arithmetic operators/script.js
@@ -0,0 +1,37 @@
+// arithmetic operators = operands (values, variables, ctc.)
+//                        operators (+, -, *, /, %, **)
+//                        ex. 11 = x + 5;
+
+let student = 30;
+
+// student = student + 1;
+// student = student - 1;
+// student = student * 1;
+// student = student / 1;
+// student = student % 1;
+// student = student ** 1;
+
+// student += 1;
+// student -= 1;
+// student *= 1;
+// student /= 1;
+// student %= 1;
+// student **= 1;
+
+// student++;
+// student--;
+
+console.log(student);
+
+
+/*
+    operator precedence
+    1. parenthsis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
