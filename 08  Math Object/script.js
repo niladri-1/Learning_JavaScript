de49032e4ab5dcef4a05760a401b2ace88cd2b7d
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-07 07:28:22 +0530

    Math object in JS

---
+++ 08  Math Object/script.js
@@ -0,0 +1,28 @@
+// Math = built-in object that provides
+//        a collection of properties and methods.
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x = 3.21;
+let y = 2;
+let w = 1;
+let z;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x, y);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+let max = Math.max(x, y, w);
+let min = Math.min(x, y, w);
+
+console.log(z);
+console.log(max);
