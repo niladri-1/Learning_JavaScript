df55fbd664925d916dfa581e244103a5a37c10c1
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-07 15:33:22 +0530

    Ternary operator in JS

---
+++ 12 Ternary operator/script.js
@@ -0,0 +1,16 @@
+// Ternary operator = a shortcut to if{} and else{} statements
+//                    helps to assign a variabele based on a condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
+
+let age = 18;
+let message1 = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message1);
+
+let isStudent = true;
+let message2 = isStudent ? "You are a student" : "You are not a student";
+console.log(message2);
+
+let amout = 120;
+let discount = amout >= 100 ? 10 : 0;
+console.log(`Your total is $ ${amout - amout * (discount / 100)}`);
