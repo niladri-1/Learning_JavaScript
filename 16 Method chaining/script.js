b7f2d4a77d07a2585dfc3be4a2dfc29e9cbc0be5
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-14 11:55:08 +0530

    Method chaining in JS

---
+++ 16 Method chaining/script.js
@@ -0,0 +1,22 @@
+// METHOD CHAINING = calling one method after another
+//                   in one continuous line of code.
+
+let userName = window.prompt("Enter Username: ");
+
+// ---------  NO METHOD CHAINING -----------
+userName = userName.trim();
+let letter = userName.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = userName.slice(1);
+extraChars = extraChars.toLowerCase();
+userName = letter + extraChars;
+
+console.log(userName);
+
+
+// ---------  METHOD CHAINING -----------
+
+userName = userName.trim().charAt(0).toUpperCase() + userName.trim().slice(1).toLowerCase();
+
+console.log(userName);
