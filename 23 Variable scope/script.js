431ef6b2696b2aac1b7b3a93aca3400a6b975bcd
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-18 20:23:31 +0530

    Local vs Global Variable

---
+++ 23 Variable scope/script.js
@@ -0,0 +1,16 @@
+// Variable scope = where a variable is recognized
+//                  and accessible (local vs global).
+
+let x = 10; // global variable
+
+function function1() {
+	x = 20; // local variable
+	console.log(x); // 20
+}
+
+function function2() {
+	console.log(x); // 10
+}
+
+function1();
+function2();
