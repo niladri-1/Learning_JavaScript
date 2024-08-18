d4b4340744464ad77217e5f9dd4eab0488886b94
Niladri Chatterjee <code.niladri@gmail.com>
2024-08-18 09:14:08 +0530

    For loops in JS

---
+++ 20 For loops/script.js
@@ -0,0 +1,15 @@
+// For loops = repeart some code a LIMIT number of times
+
+for (let i = 0; i <= 2; i++) {
+    console.log(i);
+}
+
+
+for (i = 1; i < 20; i++) {
+    if (i == 13) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
+}
