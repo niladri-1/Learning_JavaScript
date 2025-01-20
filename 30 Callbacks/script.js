10da61c042fe5db951744991a9ba3cc54bda193b
Niladri Chatterjee <code.niladri@gmail.com>
2025-01-20 22:53:52 +0530

    Callbacks in JS

---
+++ 30 Callbacks/script.js
@@ -0,0 +1,42 @@
+// Callbacks = a function that is passed as an argument
+//            to another function.
+
+
+// 				used to hendel asynchronous operations:
+// 				1. Reading a file
+// 				2. Network requests
+// 				3. Interacting with Dtabase
+
+// 				"Hey, when you're done, call this next."
+
+
+// Example 1
+hello(goodbye); // hello
+hello(leave); // good-bye
+
+
+function hello(callback) {
+	console.log("hello");
+	callback();
+}
+
+function goodbye() {
+	console.log("good-bye");
+}
+
+function leave() {
+	console.log("leave");
+}
+
+
+// Example 2
+function sum(callBack, x, y) {
+	let result = x + y;
+	callBack(result);
+}
+
+function displayConsole(result) {
+	console.log(result);
+}
+
+sum(displayConsole, 10, 20); //30
