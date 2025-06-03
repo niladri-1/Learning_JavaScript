5b6c0d627385a6150170ac424cdf4e8cf8b934d4
Niladri Chatterjee <code.niladri@gmail.com>
2025-06-03 07:12:49 +0530

    update Strings methonds in JS

--- 14 String methods/script.js
+++ 14 String methods/script.js
@@ -5,14 +5,16 @@ let date = "15-08-1947";
 
 console.log(userName.charAt(0)); //B
 console.log(userName.indexOf("o")); //2
-console.log(userName.length); //10
+console.log(userName.lastIndexOf("o")); //5
+console.log(userName.length); //8
 console.log(userName.trim()); //Bro-Code
 console.log(userName.toUpperCase()); //BRO-CODE
 console.log(userName.toLowerCase()); //bro-code
 console.log(userName.repeat(3)); //Bro-CodeBro-CodeBro-Code
-console.log(userName.startsWith("b")); //true or false
-console.log(userName.endsWith("e")); //true or false
+console.log(userName.startsWith("b")); //true or false -> false
+console.log(userName.endsWith("e")); //true or false -> true
+console.log(userName.includes(" ")); //true or false -> false
 
 console.log(date.replaceAll("-", "/")); // 15/08/1947
-console.log(date.padStart(15, "0")); // 00000015
-console.log(date.padEnd(15, "0")); // 00000000000000015
+console.log(date.padStart(15, "0")); // 00000015-08-1947
+console.log(date.padEnd(15, "0")); // 15-08-194700000
