3f87bf4dfbc927bae1d0df4f6ac52dbb0fac6f43
Niladri Chatterjee <code.niladri@gmail.com>
2025-06-03 07:15:28 +0530

    update Variables in JS

--- 02 Variables/script.js
+++ 02 Variables/script.js
@@ -35,5 +35,5 @@ let Age = 25;
 let student = false;
 
 document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p2").textContent = `You are ${Age} years old`;
 document.getElementById("p3").textContent = `Enrolled: ${student}`;
