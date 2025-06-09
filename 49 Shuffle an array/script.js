c32e419856a186e33590fd850df7872df72aa498
Niladri Chatterjee <code.niladri@gmail.com>
2025-06-09 12:24:50 +0530

    Shuffle an Array

---
+++ 49 Shuffle an array/script.js
@@ -0,0 +1,15 @@
+// Fisher-Yates
+
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+suffle(cards);
+
+function suffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const random = Math.floor(Math.random() * (i + 1));
+
+		[array[i], array[random]] = [array[random], array[i]];
+	}
+}
+
+console.log(cards);
