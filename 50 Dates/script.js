b9c69369bf21aad35c31326d920bf5347e8f9f5d
Niladri Chatterjee <code.niladri@gmail.com>
2025-06-10 08:27:16 +0530

    Dates in JS

--- 50 Dates/script.js
+++ 50 Dates/script.js
@@ -1,18 +1,22 @@
 // Date objects = objects that contain values that represend dates and times
 // 				  These date objects can be chenged and formatted
 
-const date = new Date();
-console.log(date); //2025-06-09T10:42:18.734Z
-console.log(`${date}`); //Mon Feb 03 2025 07:03:04 GMT+0530 (India Standard Time)
 
-// Date(year, month, day, hour, minute, second, ms)
-const newDate = new Date(2025, 1, 3, 7, 3, 4, 5);
-console.log(newDate); //2025-02-03T01:33:04.005Z
-console.log(`newDate:${newDate}`); //Mon Feb 03 2025 07:03:04 GMT+0530 (India Standard Time)
+// Current date & time
+const now = new Date();
+console.log(now); //2025-06-09T10:42:18.734Z
+console.log(`${now}`); //Mon Feb 03 2025 07:03:04 GMT+0530 (India Standard Time)
 
-const customDate = new Date("2024-01-02T12:00:00.000Z");
-console.log(customDate); //2024-01-02T12:00:00.000Z
-console.log(`customDate:${customDate}`); //Tue Jan 02 2024 17:30:00 GMT+0530 (India Standard Time)
+// Create specific date: Date(year, monthIndex, day, hour, min, sec, ms)
+const d1 = new Date(2025, 1, 3, 7, 3, 4, 5);
+console.log(d1); //2025-02-03T01:33:04.005Z
+console.log(`${d1}`); //Mon Feb 03 2025 07:03:04 GMT+0530 (India Standard Time)
+
+
+// Create date from string (ISO format)
+const d2 = new Date("2024-01-02T12:00:00.000Z");
+console.log(d2); //2024-01-02T12:00:00.000Z
+console.log(`${d2}`); //Tue Jan 02 2024 17:30:00 GMT+0530 (India Standard Time)
 
 
 
@@ -31,28 +35,21 @@ console.log(year, month, day, hour, minutes, seconds, dayOfWek); //2025 5 9 16 4
 
 
 
+// Set parts of a date
+const modDate = new Date();
+modDate.setFullYear(2024);
+modDate.setMonth(0);  // Jan
+modDate.setDate(2);
+modDate.setHours(3);
+modDate.setMinutes(3);
+modDate.setSeconds(4);
 
-
-const myNewDate = new Date();
-
-myNewDate.setFullYear(2024);
-myNewDate.setMonth(0); //January
-myNewDate.setDate(2); //1st day of January
-myNewDate.setHours(3);
-myNewDate.setMinutes(3);
-myNewDate.setSeconds(4);
-
-
-console.log(myNewDate); //2024-01-01T21:33:04.740Z
-console.log(`myNewDate:${myNewDate}`); //Tue Jan 02 2024 03:03:04 GMT+0530 (India Standard Time)
+console.log(modDate);
+console.log(`${modDate}`);
 
 
 
-const date1 = new Date("2024-12-31");
-const date2 = new Date("2025-01-01");
-
-if (date1 > date2) {
-	console.log(`date1 - ${date1}`);
-} if (date1 < date2) {
-	console.log(`date2:${date2}`); //Wed Jan 01 2025 05:30:00 GMT+0530 (India Standard Time)
-}
+// Compare two dates
+const dt1 = new Date("2024-12-31");
+const dt2 = new Date("2025-01-01");
+console.log(dt1 < dt2 ? `dt2: ${dt2}` : `dt1: ${dt1}`);
